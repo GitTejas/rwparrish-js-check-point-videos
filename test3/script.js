8a3bce1f2a4c4b88a7ea8d24fe39fa7600de28fb
Tejas Nampalli <TSNampalli@gmail.com>
2023-11-05 18:29:38 -0500

    finished lab and added youtube video link

--- test3/script.js
+++ test3/script.js
@@ -7,7 +7,15 @@ Then test the function to be sure that it works.
 
 Lastly, explain the difference between parameters and arguments and explain the scope of your function's parameters.
 */
-
-
   
+function sum(x=1 , y=2) {
+    return x + y
+}
+sum(4, 5)
+
+
+/*
+YouTube Submission Link Below
+https://www.youtube.com/watch?v=hrDipBW8T4U
+*/
   
